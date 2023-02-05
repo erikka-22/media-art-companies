4c6898d930a5e057704e8338160ba65c52e1128c
erikka-22 <iwasaki.pingoo@gmail.com>
2023-02-05 23:55:29 +0900

    地図を表示

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app id="inspire">
-    <!-- <v-navigation-drawer v-model="drawer"> -->
-      <!--  -->
+    <!-- <v-navigation-drawer v-model="store.state.drawer"> -->
+      
     <!-- </v-navigation-drawer> -->
 
     <v-app-bar>
-      <!-- <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon> -->
+      <v-app-bar-nav-icon @click="store.setDrawer(!store.state.drawer)"></v-app-bar-nav-icon>
 
       <v-toolbar-title>メディア芸術企業・団体マップ</v-toolbar-title>
     </v-app-bar>
@@ -17,37 +17,41 @@
   </v-app>
 </template>
 
-<!-- <script setup lang="ts"> -->
-<script setup>
+<script setup lang="ts">
+// <script setup>
   import { Loader } from '@googlemaps/js-api-loader'
+  import { MarkerClusterer } from '@googlemaps/markerclusterer'
   import { ref, reactive } from 'vue'
   import Papa from 'papaparse'
+  import { isPresent, isDefined, isFilled } from 'ts-is-present'
 
-  const drawer = ref(null)
+  interface LatLng {
+    lat: number,
+    lng: number
+  }
 
   const store = {
     state: reactive({
-      locationList: {}
+      locationList: [] as Array<LatLng>,
+      drawer: false,
     }),
-    setLocationList(newValue) {
+    setLocationList(newValue: Array<LatLng>) {
       this.state.locationList = newValue
+    },
+    setDrawer(newValue: boolean) {
+      this.state.drawer = newValue
     }
-  }
+  }  
   
-  const csvOptions = {
-    csvLat: 5,
-    csvLng: 6,
-  }
-  
-  const dropLocationPoints = () => {
-
-  }
-
-  const parseCsv = Papa.parse('./src/assets/address_withlatlng.csv', {
+  const parseCsv = Papa.parse<any>('./src/assets/address_withlatlng.csv', {
     download: true,
     header: true,
     complete: function(results){
-      store.setLocationList(results.data)
+      const latlng: Array<LatLng> = results.data.map(result => {
+        return {lat: Number(result['緯度']), lng: Number(result['経度'])}
+      })
+      console.log(latlng)
+      store.setLocationList(latlng)
     }
   })
   // console.log(store.state.locationList)
@@ -59,17 +63,27 @@
   })
 
   const mapOptions = {
+    zoom: 5,
     center: {
-      lat: 0,
-      lng: 0
-    },
-    zoom: 4
+      lat: 35.65629,
+      lng: 139.74142
+    }
   }
 
   loader
     .load()
     .then((google) => {
-      new google.maps.Map(document.getElementById("map"), mapOptions)
+      const map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement, 
+        mapOptions
+      )
+      const markers = store.state.locationList.map((position) => {
+        const marker = new google.maps.Marker({
+          position
+        })
+        return marker    
+      })
+      new MarkerClusterer({ markers, map})
     })
     .catch(e => {
 
